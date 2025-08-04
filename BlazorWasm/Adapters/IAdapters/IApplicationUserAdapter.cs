f7474dcc89c39ab6fd7f61131ec92a4c843acfc6
MasJlina <anton.omelch@gmail.com>
2025-08-04 03:34:43 +0300

    create client project

--- BlazorWasm/Adapters/IAdapters/IApplicationUserAdapter.cs
+++
@@ -1,9 +0,0 @@
-using BusinessLogic.DTOs;
-
-namespace BlazorWasm.Adapters.IAdapters;
-
-public interface IApplicationUserAdapter
-{
-    public Task<ApplicationUserDTO?> GetById(string applicationUserId);
-    public Task<bool> Update(ApplicationUserDTO applicationUserDTO);
-}
