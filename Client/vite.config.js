5611e1d62a15f8b9cdd21d6a00c5fe342ef726f6
MasJlina <anton.omelch@gmail.com>
2026-03-26 21:29:01 +0100

    add favicon and move to public dir

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -3,11 +3,18 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
+    root: "public",
+    publicDir: false,
+    envDir: path.resolve(__dirname),
     plugins: [react()],
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "src"),
         },
+    },
+    build: {
+        outDir: "../dist",
+        emptyOutDir: true,
     },
     server: {
         port: 5500
