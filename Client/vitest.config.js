a619b45a600bcdc057a43f6e1d7d8ab77c2da6ff
MasJlina <anton.omelch@gmail.com>
2026-04-22 19:53:33 +0200

    configure react testing library

--- Client/vitest.config.js
+++ Client/vitest.config.js
@@ -1,9 +1,14 @@
-import { defineConfig } from 'vitest/config';
-import path from 'path';
+import { defineConfig } from "vitest/config";
+import path from "path";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+    plugins: [react()],
     test: {
-        include: ['src/**/*.test.js'],
+        environment: "jsdom",
+        globals: true,
+        include: ["src/**/*.test.{js,jsx,ts,tsx}"],
+        setupFiles: "./setupTests.js",
     },
     resolve: {
         alias: {
